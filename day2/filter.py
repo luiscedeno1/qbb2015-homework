6e8612283e13083022d2dba405a96351d4e2be47
Luis Cedeno-Rosario <lcedeno1@jhu.edu>
2015-08-25 11:25:09 -0400

    Filter lines 10 through 15

--- day2/filter.py
+++ day2/filter.py
@@ -5,9 +5,27 @@ filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 f = open( filename )
 
 # Interate the file line by line
-for line in f:
+#for line in f:
     # Split the line on whitespace
-    fields = line.split()
+    #fields = line.split()
     # Determine if the 9th column contains the spring tRNA
-    if "tRNA" in fields[9]:
-        print line,
+    #if "tRNA" in fields[9]:
+        #print line,
+        
+#line_count = 0
+#for data in f:
+    #if line_count <= 10:
+        #pass
+    #elif line_count <= 15:
+        #print data,
+    #else:
+        #break
+    #line_count += 1
+    
+for line_count, data in enumerate( f ):
+    if line_count <= 10:
+        pass
+    elif line_count <= 15:
+        print data,
+    else:
+        break        
