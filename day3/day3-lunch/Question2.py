3fdc5464e0a2ffae449eaa6216bdcf14ac8088a8
Luis Cedeno-Rosario <lcedeno1@jhu.edu>
2015-08-27 09:55:01 -0400

    Question 2

---
+++ day3/day3-lunch/Question2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_name1 = pd.read_table("~/qbb2015/stringtie/SRR072893/t_data.ctab")
+
+#  Hay que eliminar los ceros
+df = file_name1[file_name1["FPKM"] > 0]
+
+#  Esto se usa para contruir la grafica
+plt.figure()
+plt.hist(np.log(df["FPKM"].values))
+plt.title("FPKM values in SRR072893")
+plt.ylabel("mRNA Abundance (FPKM) in SRR072893")
+plt.xlabel("log FPKM Values")
+plt.savefig("question2histogram.png")
+
+
+
+
+
+
+
+
+
+
+
